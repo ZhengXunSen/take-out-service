238ccdf423b0be88d648de79a7434171035ffb46
ZhengXunSen <1043386358@qq.com>
2020-06-15 21:14:49 +0800

    first commit

---
+++ src/main/java/com/dream/takeoutserive/TakeOutSeriveApplication.java
@@ -0,0 +1,13 @@
+package com.dream.takeoutserive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeOutSeriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeOutSeriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dream/takeoutserive/TakeOutSeriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.takeoutserive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeOutSeriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
